ca9737913b0a81af9d2c1524cb05a6203d267ee6
harshal-kaushik <kaushikharshal02@gmail.com>
2026-05-22 18:21:13 +0530

    created a rag_engine

--- backend/config.py
+++ backend/config.py
@@ -1,12 +1,10 @@
 CHUNK_SIZE = 1000
 CHUNK_OVERLAP = 200
-import os
-os.environ["GOOGLE_API_KEY"] = "AIzaSyAO_DL5b8XCrcs2nimzcBgmZu6C9GrwOiM"
 EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
 LLM_MODEL = "gemini-2.5-flash"
 
 FAISS_PATH = "faiss_index"
 
-TOP_K = 4
+TOP_K = 8
 MAX_LENGTH = 512
 TEMPERATURE = 0.3
